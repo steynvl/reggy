961101d74f75852398c864a53efcc79343d1dab2
steynvl <steyn.van.l@gmail.com>
2018-03-15 16:56:27 +0200

    Added Perl code examples

---
+++ client/app/language-examples/perl/perl-examples.ts
@@ -0,0 +1,31 @@
+
+const examples = {
+
+  'if/else branch whether the regex matches (part of) a string': 'if ($subject =~ m/__regex__/) {\n' +
+  '    # Successful match\n' +
+  '} else {\n' +
+  '    # Match attempt failed\n' +
+  '}\n',
+  'if/else branch whether the regex matches a string entirely': 'if ($subject =~ m/\\A__regex__\\z/) {\n' +
+  '    # Successful match\n' +
+  '} else {\n' +
+  '    # Match attempt failed\n' +
+  '}\n',
+  'Get the part of a string matched by the regex': 'if ($subject =~ m/__regex__/) {\n' +
+  '    $result = $&;\n' +
+  '} else {\n' +
+  '    $result = "";\n' +
+  '}\n',
+  'Get the part of a string matched by a numbered group': 'if ($subject =~ m/__regex__/) {\n' +
+  '    $result = $1;\n' +
+  '} else {\n' +
+  '    $result = "";\n' +
+  '}\n',
+  'Get an array of all regex matches in a string': '@result = $subject =~ m/__regex__/g;',
+  'Iterate over all matches in a string': 'while ($subject =~ m/__regex__/g) {\n' +
+  '    # matched text = $&\n' +
+  '}\n'
+
+};
+
+export { examples };
--- client/app/language-examples/perl/perl.component.ts
+++ client/app/language-examples/perl/perl.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { examples } from './perl-examples';
 
 @Component({
   selector: 'app-perl-code-samples',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./perl.component.css']
 })
 export class PerlComponent {
+
+  selectedExample: string;
+  examples = examples;
+  regex: string;
+
+  changed() {
+    this.regex = '^[2-9]version\\d*$';
+    examples[this.selectedExample] = examples[this.selectedExample].replace('__regex__', this.regex);
+  }
 
 }
