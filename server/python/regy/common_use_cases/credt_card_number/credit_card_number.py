0da3b01ddad931064100ea791542f1d1ff2bdf09
steynvl <steyn.van.l@gmail.com>
2018-04-04 20:30:03 +0200

    Implemented credit card numbers (common)

--- server/python/regy/common_use_cases/credt_card_number/credit_card_number.py
+++ server/python/regy/common_use_cases/credt_card_number/credit_card_number.py
@@ -1,6 +1,4 @@
-from collections import deque
-
-from regy.common_use_cases.credt_card_number.options_to_re import opt_to_re
+from regy.common_use_cases.credt_card_number.options_to_re import credit_card_to_re
 
 
 class CreditCardNumber:
@@ -8,12 +6,25 @@ class CreditCardNumber:
     def __init__(self, info, target):
         self._info = info
         self._target = target
-        self._re = deque()
+        self._re = []
         self._calculate_regex()
 
     def get_re(self):
         return ''.join(self._re)
 
     def _calculate_regex(self):
-        target = opt_to_re[self._target]
+        credit_cards = self._get_checked_credit_cards()
 
+        card_format_info = self._info['spacesAndDashes']
+        target = credit_card_to_re[self._target]
+
+        for credit_card in credit_cards:
+            self._re.append(target[credit_card][card_format_info])
+
+        if len(credit_cards) > 1:
+            self._re = ['|'.join(self._re)]
+
+    def _get_checked_credit_cards(self):
+        return [
+            k for k in self._info.keys() if k != 'spacesAndDashes' and self._info[k]
+        ]
